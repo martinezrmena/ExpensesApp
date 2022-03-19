b0fe9fb559672ac0b15c2e85e8720c5ece863174
Rafael Martinez <rafael@megsoftconsulting.com>
2022-03-18 18:27:12 -0600

    Visual State Manager.

--- ExpensesApp/Views/CategoriesPage.xaml.cs
+++ ExpensesApp/Views/CategoriesPage.xaml.cs
@@ -13,6 +13,7 @@ namespace ExpensesApp.Views
         {
             InitializeComponent();
             ViewModel = Resources["vm"] as CategoriesVM;
+            SizeChanged += CategoriesPage_SizeChanged;
         }
 
         protected override void OnAppearing()
@@ -20,6 +21,23 @@ namespace ExpensesApp.Views
             base.OnAppearing();
 
             ViewModel.GetExpensesPerCategory();
+        }
+
+        void CategoriesPage_SizeChanged(object sender, EventArgs e)
+        {
+            string visualState = Width > Height ? "Landscape" : "Portrait";
+
+            VisualStateManager.GoToState(titleLabel, visualState);
+        }
+
+        void Handle_Pressed(object sender, System.EventArgs e)
+        {
+            VisualStateManager.GoToState(exampleButton, "Focused");
+        }
+
+        void Handle_Released(object sender, System.EventArgs e)
+        {
+            VisualStateManager.GoToState(exampleButton, "Normal");
         }
     }
 }
